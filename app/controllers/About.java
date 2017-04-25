ed9ef2330a70791b5ad45b39a95ae617557d565f
Bernie <bernieh1012008@live.co.uk>
2017-04-25 11:28:35 +0100

    first commit

--- app/controllers/About.java
+++ app/controllers/About.java
@@ -5,10 +5,9 @@ import play.mvc.*;
 import java.util.*;
 import models.*;
 
-public class About extends Controller
-{
+public class About extends Controller {
+
   public static void index() {
-    Logger.info("Rendering about");
-    render ("about.html");
+    render();
   }
 }
